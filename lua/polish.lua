c6b5c9cf5c8506f28c879de1a5dd13d8230da77b
Peter <lilv447@gmail.com>
2025-07-27 01:56:23 -0400

    updated color colomn length

--- lua/polish.lua
+++ lua/polish.lua
@@ -2,5 +2,5 @@
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
-vim.opt.colorcolumn = { "72", "80" }
+vim.opt.colorcolumn = { "72", "88" }
 vim.notify = require("noice").notify
